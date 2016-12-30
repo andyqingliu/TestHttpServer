b352b1cf8694034abef9e011ac4be8dd0fa3cc0a
andyqingliu <andyqingliu@163.com>
2016-12-30 10:18:44 +0800

    Modify MyHttpSrv.java

--- TestHttpServer/src/TestHttpSrv/MyHttpSrv.java
+++ TestHttpServer/src/TestHttpSrv/MyHttpSrv.java
@@ -25,8 +25,6 @@ public class MyHttpSrv {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-		System.out.println("My First Java App!");
-		
 		final InetSocketAddress sa = new InetSocketAddress(8888);
 		HttpServer server = null;
 		try {
